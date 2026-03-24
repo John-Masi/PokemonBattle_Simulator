752a81e5c4201b519aedee4a01b6cfdb92426a46
John Masi <johnrmasi@gmail.com>
2026-03-24 18:35:00 -0500

    Add files via upload

---
+++ build/CMakeFiles/pokemon.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/johnmasi/pokemon/src/battle.cpp" "CMakeFiles/pokemon.dir/src/battle.o" "gcc" "CMakeFiles/pokemon.dir/src/battle.o.d"
+  "/home/johnmasi/pokemon/src/trainer.cpp" "CMakeFiles/pokemon.dir/src/trainer.o" "gcc" "CMakeFiles/pokemon.dir/src/trainer.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pokemon.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pokemon.dir/src/battle.o"
+  "CMakeFiles/pokemon.dir/src/battle.o.d"
+  "CMakeFiles/pokemon.dir/src/trainer.o"
+  "CMakeFiles/pokemon.dir/src/trainer.o.d"
+  "pokemon"
+  "pokemon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pokemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
